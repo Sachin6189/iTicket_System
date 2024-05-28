e866abccba1bd2ecbfabb61fe293635e6c27d999
Sachin Kumar (TTL) <sk928810.ttl@tatamotors.com>
2024-05-28 09:56:17 +0530

    updated routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,8 @@ import Login from "../components/Login/Login";
 import Dashboard from "../components/Dashboard/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RaiseTicket from "../components/Dashboard/RaiseTicket";
-import { LoginProvider } from "../LoginContext"; 
+import { LoginProvider } from "../LoginContext";
+import ProtectedRoute from '../ProtectedRoute'; 
 
 const App = () => {
   return (
@@ -11,8 +12,8 @@ const App = () => {
       <LoginProvider>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/dashboard/" element={<Dashboard />} />
-          <Route path="/dashboard/raiseTicket/" element={<RaiseTicket />} />
+          <Route path="/dashboard/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/dashboard/raiseTicket/" element={<ProtectedRoute><RaiseTicket /></ProtectedRoute>} />
         </Routes>
       </LoginProvider>
     </Router>
